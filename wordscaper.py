a9e4271d4f5646ce250297854c0fe6c42ccce774
Jonathan <jonathan.nam14@gmail.com>
2020-02-27 00:13:46 -0500

    added words_alpha and main script

---
+++ wordscaper.py
@@ -0,0 +1,31 @@
+import sys
+from itertools import permutations
+
+MIN_WORD_LEN = 3
+
+if __name__ == "__main__":
+	f = open('words_alpha.txt', 'r')
+	
+	chars = sys.argv[1]
+	max_word_len = int(sys.argv[2])
+	
+	all_permutations = []
+	for i in range(MIN_WORD_LEN, max_word_len, 1):
+		all_permutations += [''.join(p) for p in permutations(chars, i)]
+	
+	possible_words = []
+	for x in f:
+		word = x.strip()
+		if word in all_permutations:
+			possible_words.append(word)
+			
+	f.close()
+	
+	# Sort by length
+	# And then by alphabetical order
+	
+	possible_words.sort()
+	possible_words.sort(key=len)
+	
+	for word in possible_words:
+		print(word)
